61f421888c0abd3ce287e71ec0e4c03d9e912c20
hatahilaa <hatahila@gmail.com>
2018-12-21 17:32:38 +0200

    remove sensitive data

---
+++ .history/config_20181221173200.php
@@ -0,0 +1,8 @@
+<?php
+//sendGrid
+define('HILAIRE_SENDGRID_API_KEY', '#########################################################');
+//publisher constants
+define('APP_ID','#############');
+define('APP_KEY','##############');
+define('APP_SECRET','###################');
+define('APP_CLUSTER','############');
